34929aa84c69920da78859be8c81773c4b56d834
Vince Cotter <vince@currant.com>
2018-11-20 15:57:09 -0800

    Added support for wifi operations

--- main/powertest.c
+++ main/powertest.c
@@ -9,13 +9,54 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
 #include <string.h>
 #include "nvs.h"
 #include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "tcpip_adapter.h"
+#include "esp_event_loop.h"
 
 // Omar headers:
 #include "hw_setup.h"
-#include "cmd_decl.h"
+
+static EventGroupHandle_t wifi_event_group;
+static const int CONNECTED_BIT = BIT0;
+static const int DISCONNECTED_BIT = BIT1;
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+        xEventGroupClearBits(wifi_event_group, DISCONNECTED_BIT);
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+static void initialise_wifi(void)
+{
+    static bool initialized = false;
+    if (initialized) {
+        return;
+    }
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    initialized = true;
+}
 
 
 static void initialize_nvs()
@@ -30,17 +71,17 @@ static void initialize_nvs()
 
 void powertest(void)
 {
-        printf("powertest started:\n");
+    printf("powertest started:\n");
 
     initialize_nvs();
     omar_setup();
     initialise_wifi();
 
 
-        while (1){
-                printf("powertesting...\n");
-                vTaskDelay(1000/portTICK_PERIOD_MS);
-        }
+    while (1){
+        printf("powertesting...\n");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
 
 }
 
