571b0e95934d31495072b9a3523478219cd4990b
Vince Cotter <vince@currant.com>
2018-11-20 15:42:24 -0800

    Now powertest sits in an infinite loop doing nothin'

--- main/powertest.c
+++ main/powertest.c
@@ -10,15 +10,37 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <string.h>
+#include "nvs.h"
+#include "nvs_flash.h"
+
+// Omar headers:
+#include "hw_setup.h"
+#include "cmd_decl.h"
+
+
+static void initialize_nvs()
+{
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+}
 
 void powertest(void)
 {
-	printf("powertest started:\n");
+        printf("powertest started:\n");
 
-	while (1){
-		printf("powertesting...\n");
-		vTaskDelay(1000/portTICK_PERIOD_MS);
-	}
+    initialize_nvs();
+    omar_setup();
+    initialise_wifi();
+
+
+        while (1){
+                printf("powertesting...\n");
+                vTaskDelay(1000/portTICK_PERIOD_MS);
+        }
 
 }
 
